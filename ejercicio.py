65503f15c0514838f73bc39b427e4603c234724d
joacoLoberza <j.loberza@alumno.etec.um.edu.ar>
2025-04-09 12:39:11 -0300

    Nueva función sacar perímetro cudrado, archivo ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -1,6 +1,13 @@
 def circlePerimeter (radius):
     perimeter = 2*3.1416*radius
     return perimeter
+def squerePerimeter (side):
+    perimeter = 4*side
+    return perimeter
 radius = float(input("Introduzca el radio del círculo:\n"))
 perimeter = circlePerimeter(radius)
 print("El perímetro del círculo es: ", perimeter)
+
+side = float(input("Introduzca el lado del cuadrado:\n"))
+perimeter = squerePerimeter(side)
+print("El perímetro del cuadrado es: ", perimeter)
